ccfc49a0b9299cf01c31971ca4fc9f24a905ae9e
Jose Shardin <joseruiz.sh@gmail.com>
2024-08-12 17:20:22 -0500

    primer carrito de compras en react + vite

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CaritoApp } from "./CaritoApp";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <CaritoApp />
+    </React.StrictMode>
+    ,
+  </BrowserRouter>
+);
